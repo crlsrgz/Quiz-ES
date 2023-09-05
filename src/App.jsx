397926819e7ef2a47c781104bbadc2e2c8628cb5
crlsrgz <crls@fake>
2023-09-05 21:26:30 +0200

    style: structuring page with longer content

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import names from "./data/names";
 
 export default function App() {
   return (
@@ -7,13 +8,23 @@ export default function App() {
         <div className="container m-12 mx-auto flex w-4/5 flex-col justify-center gap-8">
           <h1 className="title text-center font-semibold">CrlsRgz</h1>
           <div className="mx-auto">
-            <h2 className="font-semibold">&quot; Text Content &quot;</h2>
+            <h2 className="mt-8 text-center font-serif font-semibold">
+              &quot; Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Blanditiis adipisci tenetur sit fugit, labore commodi hic? Modi,
+              sunt beatae. Minus. &quot;
+            </h2>
           </div>
-          <div className="mx-auto flex flex-col gap-4">
-            <h3 className="font-semibold">More Content</h3>
-            <h3 className="font-semibold">More Content</h3>
-            <h3 className="font-semibold">More Content</h3>
-            <h3 className="font-semibold">More Content</h3>
+          <div className="mx-auto mt-8 flex flex-col gap-4">
+            {names.map((name, index) => {
+              return (
+                <h3
+                  key={index}
+                  className="m-0 mt-4 h-12 cursor-pointer p-0 text-center text-slate-700 hover:text-slate-900 sm:mt-8"
+                >
+                  {name}
+                </h3>
+              );
+            })}
           </div>
         </div>
       </div>
