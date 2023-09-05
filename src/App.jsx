fc8981f9c581d02df4822f62504b186ed80d2e9c
crlsrgz <c@c.com>
2023-09-05 17:28:12 +0200

    style: basic structuring

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,18 @@ import React from "react";
 export default function App() {
   return (
     <>
-      <div className="main-container m-0 h-screen p-0">
-        <div className="container m-12">
-          <h1 className="title text-8xl font-semibold">CrlsRgz</h1>
+      <div className="main-container m-0 h-screen p-0 ">
+        <div className="container m-12 mx-auto flex w-4/5 flex-col justify-center gap-8">
+          <h1 className="title text-center font-semibold">CrlsRgz</h1>
+          <div className="mx-auto">
+            <h2 className="font-semibold">&quot; Text Content &quot;</h2>
+          </div>
+          <div className="mx-auto flex flex-col gap-4">
+            <h3 className="font-semibold">More Content</h3>
+            <h3 className="font-semibold">More Content</h3>
+            <h3 className="font-semibold">More Content</h3>
+            <h3 className="font-semibold">More Content</h3>
+          </div>
         </div>
       </div>
     </>
