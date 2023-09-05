949f10cb189a494fbf6473697a7b3d9f47714475
crlsrgz <c@c.com>
2023-09-05 12:52:31 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App(){
+    return (
+        <>
+            <div className="main-container p-0 m-0 bg-red-50">
+                <h1 className="title text-8xl">CrlsRgz</h1>
+            </div>
+        </>
+    )
+}
