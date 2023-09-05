8e6ab06ef2deca2202e841f131ceb592955083f3
crlsrgz <c@c.com>
2023-09-05 13:31:30 +0200

    style: changed css classes in App and <body>

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-export default function App(){
-    return (
-        <>
-            <div className="main-container p-0 m-0 bg-red-50">
-                <h1 className="title text-8xl">CrlsRgz</h1>
-            </div>
-        </>
-    )
+export default function App() {
+  return (
+    <>
+      <div className="main-container m-0 h-screen p-0">
+        <div className="container m-12">
+          <h1 className="title text-8xl font-semibold">CrlsRgz</h1>
+        </div>
+      </div>
+    </>
+  );
 }
