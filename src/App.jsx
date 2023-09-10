240292f6a31c299fc9f3cbc1a80e9c3aeaee2fb7
crlsrgz <crls@fake>
2023-09-10 14:16:42 +0200

    test: tailwindcss applied to check structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import names from "./data/names";
 import Name from "./components/name.component";
+import Navigation from "./components/navigation.component";
 
 export default function App() {
   return (
     <>
+      <Navigation />
       <div className="main-container m-0 h-screen p-0 ">
         <div className="container m-12 mx-auto flex w-4/5 flex-col justify-center gap-8">
-          <h1 className="title text-center font-semibold">CrlsRgz</h1>
           <div className="mx-auto">
-            <h2 className="mt-8 text-center font-serif font-semibold">
+            <h1 className="mt-8 text-left font-sans font-semibold text-blue-100">
               &quot; Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Blanditiis adipisci tenetur sit fugit, labore commodi hic? Modi,
               sunt beatae. Minus. &quot;
-            </h2>
+            </h1>
           </div>
-          <div className="mx-auto mt-6 flex flex-col gap-4">
+          <div className="mx-auto mt-6 flex w-7/12 flex-col gap-4">
             {names.map((name, index) => {
               return <Name name={name} key={index} />;
             })}
--- src/components/name.component.jsx
+++ src/components/name.component.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 export default function Name(props) {
   return (
-    <h3
-      key={props.index}
-      className="m-0 mt-2 h-12 cursor-pointer p-0 text-center text-slate-700 hover:text-slate-900 sm:mt-4"
-    >
-      {props.name}
-    </h3>
+    <div className="block w-full cursor-pointer rounded-full border-2  border-slate-50 bg-slate-900 text-slate-50 hover:border-slate-50 hover:bg-slate-800 hover:text-slate-50  sm:mt-4">
+      <h3 key={props.index} className="m-0 mt-2 h-12  p-0 text-center">
+        {props.name}
+      </h3>
+    </div>
   );
 }
