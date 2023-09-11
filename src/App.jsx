5b562098f235e814057b730fb5ab173d87b81bfc
crlsrgz <crls@fake>
2023-09-11 21:46:32 +0200

    test: hidding sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+import { useState } from "react";
+import names from "./data/names";
+import Name from "./components/name.component";
+
 import Navigation from "./components/navigation.component";
 import SectionText from "./components/section.text.component";
 import BioScore from "./components/section.bioScore.component";
@@ -7,11 +11,20 @@ import Intro from "./components/section.intro.component";
 import Info from "./components/section.info.component";
 
 export default function App() {
+  const [displayQuizContainer, setDisplayQuizContainer] = useState("flex");
+  function changeQuizContainerVisibility() {
+    if (displayQuizContainer === "flex") {
+      setDisplayQuizContainer("hidden");
+    } else {
+      setDisplayQuizContainer("flex");
+    }
+  }
   return (
     <>
       <Navigation />
-      <div className="main-container m-0 h-5/6 p-0 ">
-        <SectionText classes={""} />
+
+      <div className="main-container m-0 h-5/6 p-0">
+        <SectionText classes={displayQuizContainer} />
         <BioScore classes={"hidden"} />
         <Intro classes={"hidden"} />
         <Info classes={"hidden"} />
--- src/components/name.component.jsx
+++ src/components/name.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 export default function Name(props) {
   return (
-    <div className="block w-full cursor-pointer rounded-full border-2  border-slate-50 bg-slate-900 text-slate-50 hover:border-slate-50 hover:bg-slate-800 hover:text-slate-50  sm:mt-4">
+    <div className="block w-full cursor-pointer rounded-full border-2  border-slate-50 bg-slate-800 text-slate-50 hover:border-slate-50 hover:bg-slate-700 hover:text-slate-50  sm:mt-4">
       <h3 key={props.index} className="m-0 mt-2 h-12  p-0 text-center">
         {props.name}
       </h3>
