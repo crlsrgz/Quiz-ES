c78674a6f7cced3b504dd94e850c1865929be783
crlsrgz <crls@fake>
2023-09-11 22:00:30 +0200

    style: navigation color test

--- src/components/navigation.component.jsx
+++ src/components/navigation.component.jsx
@@ -2,15 +2,15 @@ import React from "react";
 export default function Navigation() {
   return (
     <nav className="container mx-auto flex items-center justify-between">
-      <div className="logo">CrlsRgz</div>
+      <div className="logo text-blue-200 hover:text-blue-100">CrlsRgz</div>
       <ul className="flex items-center">
         <li className="mr-6">
-          <a href="/" className="text-gray-700 hover:text-gray-900">
+          <a href="/" className="text-blue-200 hover:text-blue-100">
             Home
           </a>
         </li>
         <li className="mr-6">
-          <a href="/" className="text-gray-700 hover:text-gray-900">
+          <a href="/" className="text-blue-200 hover:text-blue-100">
             About
           </a>
         </li>
