635741972f327412744f63823eeeba739ca04c45
crlsrgz <course@carlosfx.com>
2023-09-20 16:43:31 +0200

    chore: tailwind setting hoverOnlyWhenSupported

--- src/components/name.component.jsx
+++ src/components/name.component.jsx
@@ -7,8 +7,8 @@ export default function Name(props) {
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
 
     <button
-      className={`font-alata  m-auto flex h-10 w-full cursor-pointer flex-row items-center  justify-center rounded-full
-      border-2 p-0 text-center text-xl  transition   duration-500 hover:border-zinc-700  hover:bg-zinc-300 hover:text-zinc-700 md:mt-2 md:w-2/5
+      className={`m-auto  flex h-10 w-full cursor-pointer flex-row items-center justify-center  rounded-full border-2
+      p-0 text-center font-alata text-xl  transition   duration-500 hover:border-zinc-700  hover:bg-zinc-300 hover:text-zinc-700 md:mt-2 md:w-2/5
         ${props.answerClasses} ${props.classesLocalStorage}
         `}
       onClick={props.checkAnswer}
