09e7d5cfafc76c4f703183ba1bb59fe0af1943c2
crlsrgz <course@carlosfx.com>
2023-10-18 17:53:35 +0200

    style: menu responsive

--- src/components/navigation.component.jsx
+++ src/components/navigation.component.jsx
@@ -49,35 +49,41 @@ export default function Navigation() {
 
         {/* Collapsable */}
         <button
-          className=" md:hidden "
+          className=" w- md:hidden "
           data-collapse-toggle="mobile-menu"
           aria-controls="mobile-menu"
           onClick={toogleMenu}
         >
-          HEllo
+          menú
         </button>
         <ul
-          className={`absolute right-1 top-12 flex-col items-end justify-end text-base transition-transform md:hidden ${
+          className={`absolute top-12 m-auto w-11/12 flex-col items-center justify-end gap-4 border-t-4 border-zinc-200 bg-zinc-900 pt-4 text-base transition-transform md:hidden ${
             toggled ? "hidden" : " flex"
           }`}
           id="mobile-menu"
         >
           {linksArray.map((element, index) => {
             return (
-              <li key={index} className=" mr-4 flex justify-center text-center">
-                <NavLink
-                  to={`/${element}`}
-                  className={({ isActive, isPending }) =>
-                    isPending
-                      ? "m-auto border-b-4 border-zinc-900 px-1 text-red-500 hover:bg-zinc-200 hover:text-zinc-900 md:px-2"
-                      : isActive
-                      ? "m-auto border-b-4 px-1 text-red-500 md:px-2"
-                      : "m-auto border-b-4 border-zinc-900 px-1 text-red-500  hover:bg-zinc-200 hover:text-zinc-900 md:px-2"
-                  }
+              <>
+                <li
+                  key={index}
+                  className=" mr-4 flex flex-col items-center justify-center text-center"
                 >
-                  <span>{element === "" ? "Home" : element}</span>
-                </NavLink>
-              </li>
+                  <NavLink
+                    to={`/${element}`}
+                    className={({ isActive, isPending }) =>
+                      isPending
+                        ? "m-auto border-b-4  border-zinc-900 px-1  hover:bg-zinc-200 hover:text-zinc-900 md:px-2"
+                        : isActive
+                        ? "m-auto border-b-4  px-1  md:px-2"
+                        : "m-auto border-b-4  border-zinc-900 px-1   hover:bg-zinc-200 hover:text-zinc-900 md:px-2"
+                    }
+                  >
+                    <span>{element === "" ? "Home" : element}</span>
+                  </NavLink>
+                </li>
+                <span className="w-4/5 border-b-2 border-zinc-700"></span>
+              </>
             );
           })}
         </ul>
