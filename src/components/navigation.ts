c5651985324d031d4170cc8348eb81af5937b781
crlsrgz <course@carlosfx.com>
2024-05-16 20:37:18 +0200

    front: scores

--- src/components/navigation.ts
+++ src/components/navigation.ts
@@ -1,7 +1,9 @@
 export class ComponentNavigation extends HTMLElement {
     connectedCallback(word: string = "Quiz") {
         this.innerHTML = `
-        <h1 class="page-title">${word}</h1>
+        <h1 class="page-title">
+            <a href="/"> ${word} </a>
+        </h1>
         <nav-bar class="nav-desk">
             <a href="/quiz/">quiz</a>
             <a href="/score/">score</a>
